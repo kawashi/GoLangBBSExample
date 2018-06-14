4ce42f59c1ebc2f27ac3a1380511c092ccf2e076
kawashi <aisatu_nomahou@yahoo.co.jp>
2018-06-14 19:04:13 +0900

    first init

---
+++ web/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Go API server example.</h1>
+    <BBS/>
+  </div>
+</template>
+
+<script>
+import BBS from './components/BBS'
+
+export default {
+  name: 'App',
+  components: {
+    BBS
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ web/src/components/BBS.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div>
+      <input v-model="message" type="text">
+      <button v-on:click="submit">送信</button>
+    </div>
+    <div>
+      <ul>
+        <li v-for="content in contents">
+          {{content}}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BBS',
+    data () {
+      return {
+        contents: []
+      }
+    },
+    state: {
+      message: ''
+    },
+    methods: {
+      submit: function(event) {
+        // TODO: 送信クリック後の処理
+        // TODO: 1. 画面に反映
+        // TODO: 2. GoのAPIの/user_postsにPOST
+        if (this.message === '') return;
+        this.contents.unshift(this.message);
+        this.message = '';
+        vue.$forceUpdate();
+      }
+    },
+    created: function() {
+      // TODO: 初期化処理
+      // TODO: 1. GoのAPIの/user_postsをGETして、contentsに反映
+      this.contents.unshift("1件目の投稿");
+      this.contents.unshift("2件目の投稿");
+    }
+  }
+</script>
+
+<style>
+  li {
+    list-style: none;
+  }
+</style>
